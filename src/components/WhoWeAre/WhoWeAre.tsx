412137573a95cae85dc6e45e74669919eab3be1d
Daniel Hernandez <danielmark252525@gmail.com>
2022-03-26 16:09:41 -0600

    update

--- src/components/WhoWeAre/WhoWeAre.tsx
+++ src/components/WhoWeAre/WhoWeAre.tsx
@@ -1,5 +1,5 @@
 // Outside packages
-import react from 'react';
+import react, { useState } from 'react';
 
 // Components
 import InfoCard from '../InfoCard';
@@ -11,7 +11,7 @@ const WhoWeAre = () => {
   // ====================
   // State
   // ====================
-
+  const [toggleType, setToggleType] = useState('whoweare');
   // ====================
   // Variables
   // ====================
@@ -59,10 +59,6 @@ const WhoWeAre = () => {
     },
   ];
   // ====================
-  // State
-  // ====================
-
-  // ====================
   // Handle Methods
   // ====================
 
@@ -81,33 +77,67 @@ const WhoWeAre = () => {
       );
     });
   };
+
   // ====================
   // Return
   // ====================
 
   return (
     <div>
-      <h1>Who We Are</h1>
+      <button onClick={() => setToggleType('whoweare')}>Who We Are</button>
+      <button onClick={() => setToggleType('ourstory')}>Our Story</button>
 
-      <p>
-        KAD Consulting is a full-stack ERP development and strategy partner for
-        growing brands. We aim to give them access to advanced technology and
-        the expertise to implement it so they can compete with the largest
-        companies out there without fear of having an ecosystem that can't keep
-        up.
-      </p>
+      {toggleType === 'whoweare' && (
+        <>
+          <p>
+            KAD Consulting is a full-stack ERP development and strategy partner
+            for growing brands. We aim to give them access to advanced
+            technology and the expertise to implement it so they can compete
+            with the largest companies out there without fear of having an
+            ecosystem that can't keep up.
+          </p>
 
-      <span>KAD specializes in:</span>
-      <ul>
-        <li> Full-stack web and mobile experiences</li>
-        <li> Business applications</li>
-        <li> Technical architecture implementation</li>
-        <li> Data collection and aggregation</li>
-        <li> Automation</li>
-      </ul>
+          <span>KAD specializes in:</span>
+          <ul>
+            <li> Full-stack web and mobile experiences</li>
+            <li> Business applications</li>
+            <li> Technical architecture implementation</li>
+            <li> Data collection and aggregation</li>
+            <li> Automation</li>
+          </ul>
 
-      <h1>Our Values</h1>
-      <div className="who-we-are__info-cards-wrapper">{displayInfoCards()}</div>
+          <h1>Our Values</h1>
+          <div className="who-we-are__info-cards-wrapper">
+            {displayInfoCards()}
+          </div>
+        </>
+      )}
+      {toggleType === 'ourstory' && (
+        <>
+          <p>
+            KAD Consulting is made up of brilliant minds that have worked with
+            Fortune 500 & 100 technology companies. We all share a common
+            thought: what if we brought enterprise technology experience to
+            growing brands so they can bridge the gap between where they are and
+            where they would like to do business? Whether you're a visionary who
+            aims to create a better world, a company that provides vital good
+            and services, or a creator that is just starting your journey,
+            technology should never stand between you and your goals. Sure, you
+            can find easy solutions to get started, but that doesn’t mean it
+            solves all your needs. Nor does it mean that you’ll come close to
+            utilizing all its features. With all the clutter, tech can feel
+            cumbersome. Maybe you’re looking to scale your business but have no
+            idea if the “enterprise” solutions are meant for your brand's
+            future. If you're looking to invest in scaling your business, you
+            have to ensure it's time and money well spent for the road ahead and
+            not just a new problem. We aim to close the gap between your brand
+            and enterprises by providing you the technological expertise they've
+            had access to for far too long. Our mission is simple: It’s time to
+            level the playing field.
+          </p>
+          <img src="" alt="" />
+        </>
+      )}
     </div>
   );
 };
