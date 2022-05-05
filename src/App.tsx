db03299a56fb89963dc6b7495a31fa29e2f077f2
Daniel Hernandez <danielmark252525@gmail.com>
2022-05-05 15:05:52 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,28 @@ import {
   ToolsOfTheTrade,
   ContactUs,
 } from './components';
-
+// Hooks
+import useWindowDimensions from './hooks/useWindowDimensions';
 // Styles
 import './App.scss';
 
 const App = () => {
-  return (
-    <div className="app">
-      <Nav></Nav>
-      <Hero></Hero>
-      <WhoWeAre />
-      <WhatWeDo />
-      <OurProcess />
-      <ToolsOfTheTrade />
-      <ContactUs />
-    </div>
-  );
+  const { width } = useWindowDimensions();
+  if (width > 600) {
+    return (
+      <div className="app">
+        <Nav></Nav>
+        <Hero></Hero>
+        <WhoWeAre />
+        <WhatWeDo />
+        <OurProcess />
+        <ToolsOfTheTrade />
+        <ContactUs />
+      </div>
+    );
+  } else {
+    return <div>Mobile site not ready</div>;
+  }
 };
 
 export default App;
