2e48b4ce8b22b700f9fc822bef0746b13202282f
Daniel Hernandez <danielmark252525@gmail.com>
2022-05-19 14:04:18 -0600

    update

--- src/components/OurStory/OurStory.tsx
+++ src/components/OurStory/OurStory.tsx
@@ -3,10 +3,6 @@ import react, { useState } from 'react';
 
 // Components
 
-// Images
-import Daniel from '../../images/Daniel.jpeg';
-import Keegan from '../../images/Keegan.jpeg';
-
 // Styles
 import './OurStory.scss';
 
@@ -69,8 +65,14 @@ const OurStory = () => {
         <p>Our mission is simple: It’s time to level the playing field.</p>
         <h1> Our founders</h1>
         <div className="our-story__our-founders-image-wrapper">
-          <img src={Daniel} alt="" />
-          <img src={Keegan} alt="" />
+          <img
+            src="https://kad-website.s3.us-west-1.amazonaws.com/Daniel.jpeg?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEBwaCXVzLXdlc3QtMSJIMEYCIQDDYxQmYM73dGhHkIUy3HAOfWbM0bg4R3gEHjD15z4oJgIhAPcG%2FnZGZBuOlAe0yqoFcxCPKzJ619l%2BnHlnecF9W22yKu0CCPX%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQAxoMNTE0MjYwOTkzMjE5IgygFm9%2FQGPGdEkzzvYqwQJFM6dieWkHO%2Br%2BMM7Ly1o7QDKTXfE4PMOLcHj8PeeBFGHKdpTUjRY2ej%2BfZs7EE29rg4P9G0I4dGqgJbUO2%2FeL2JS9KBye%2BoJdYqAU50IvYMj%2FqiNryGValSt2mtlcHJl9nJISPd7QleyJ8t5PlaBFIipzmiARvM2K1xtqTqgTZ8A3E7v9GKpRfcEPkHrTHTmOfxK4w9RahbcaCqQxMqpe8Dj2Z9XVkhU6nYeQpRDTYYFB1WZDl9OkcXr752CySd2A19T7VpFFT1Q3SrJ1QuWB8MT28vVbq5goHpAcTjhKAOtO3hogvcc%2FJNm0BD0YOLqExsqIPv%2FxtCFdwvzNCKPY7CGXX%2Ba6%2F4H72LJt8FZS1zBRhJr6jdTpScjn35St1%2BlTe617CBQd4u2lhNGvnfnyb50c4zmWM39ZEOa9tf2NAhww8cGalAY6sgKXEJVNAAcMYszk6J4QmyOZcd6K5p0XtY4SHQCcjMEWWyvAJObGMdvAABCydNTBNrah2v8hXOimYYf6xBJ63%2B5ExAGjzE9yYVS8WnOKuZ7pfFZtnKC2wetT7lVZfWQw3ty%2Fr35pVELlZWbqxDAM3yj0IGgXVx6h9KC5hhmLeD23Xl%2B32%2FSzqz%2F1dHwyAYKFWZU4GbMyDjNLJMEGKCoMq2SAfbeEX7CjWsJYA8jQhnXtMe7MWbsCIWHMHgoITSArigSaP5TihrCQVA9TUnfQW6ALh4DVdU8X%2FK7HJAhrY20RsgSr8LEPPe8Ipj%2BDoatYEWw3XMjvdF0hL%2FMeiYoUAzdNW6LR%2FgcPENSoAHU69ejFbEsnepnWehqqkhKV5oecy2ELwxY%2B1GrK2x9gFvFFbotRfxM%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220519T200220Z&X-Amz-SignedHeaders=host&X-Amz-Expires=299&X-Amz-Credential=ASIAXPPCYATBUJXXLD4I%2F20220519%2Fus-west-1%2Fs3%2Faws4_request&X-Amz-Signature=544d7843ae104b336d834df758c057a624a5d9ba48b868721323842a43ced5f3"
+            alt="image of daniel hernandez"
+          />
+          <img
+            src="https://kad-website.s3.us-west-1.amazonaws.com/Keegan.jpeg?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEBwaCXVzLXdlc3QtMSJIMEYCIQDDYxQmYM73dGhHkIUy3HAOfWbM0bg4R3gEHjD15z4oJgIhAPcG%2FnZGZBuOlAe0yqoFcxCPKzJ619l%2BnHlnecF9W22yKu0CCPX%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQAxoMNTE0MjYwOTkzMjE5IgygFm9%2FQGPGdEkzzvYqwQJFM6dieWkHO%2Br%2BMM7Ly1o7QDKTXfE4PMOLcHj8PeeBFGHKdpTUjRY2ej%2BfZs7EE29rg4P9G0I4dGqgJbUO2%2FeL2JS9KBye%2BoJdYqAU50IvYMj%2FqiNryGValSt2mtlcHJl9nJISPd7QleyJ8t5PlaBFIipzmiARvM2K1xtqTqgTZ8A3E7v9GKpRfcEPkHrTHTmOfxK4w9RahbcaCqQxMqpe8Dj2Z9XVkhU6nYeQpRDTYYFB1WZDl9OkcXr752CySd2A19T7VpFFT1Q3SrJ1QuWB8MT28vVbq5goHpAcTjhKAOtO3hogvcc%2FJNm0BD0YOLqExsqIPv%2FxtCFdwvzNCKPY7CGXX%2Ba6%2F4H72LJt8FZS1zBRhJr6jdTpScjn35St1%2BlTe617CBQd4u2lhNGvnfnyb50c4zmWM39ZEOa9tf2NAhww8cGalAY6sgKXEJVNAAcMYszk6J4QmyOZcd6K5p0XtY4SHQCcjMEWWyvAJObGMdvAABCydNTBNrah2v8hXOimYYf6xBJ63%2B5ExAGjzE9yYVS8WnOKuZ7pfFZtnKC2wetT7lVZfWQw3ty%2Fr35pVELlZWbqxDAM3yj0IGgXVx6h9KC5hhmLeD23Xl%2B32%2FSzqz%2F1dHwyAYKFWZU4GbMyDjNLJMEGKCoMq2SAfbeEX7CjWsJYA8jQhnXtMe7MWbsCIWHMHgoITSArigSaP5TihrCQVA9TUnfQW6ALh4DVdU8X%2FK7HJAhrY20RsgSr8LEPPe8Ipj%2BDoatYEWw3XMjvdF0hL%2FMeiYoUAzdNW6LR%2FgcPENSoAHU69ejFbEsnepnWehqqkhKV5oecy2ELwxY%2B1GrK2x9gFvFFbotRfxM%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220519T200116Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAXPPCYATBUJXXLD4I%2F20220519%2Fus-west-1%2Fs3%2Faws4_request&X-Amz-Signature=d67a34601a123f2d5aa78d86c2038fd727ab8367deff290d84247b442bdfee3b"
+            alt="Image of Keegan Lamar"
+          />
         </div>
       </div>
     </div>
